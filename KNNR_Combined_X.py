7d050ae123c750df9d824e907fdb236161ccf518
hamidib <bhamidi@csu.fullerton.edu>
2016-10-15 23:37:49 -0700

    Create KNNR_Combined_X.py

---
+++ KNNR_Combined_X.py
@@ -0,0 +1,27 @@
+# CPSC483_KNNR
+from sklearn.neighbors import KNeighborsRegressor
+import numpy as np
+import urllib
+import urllib.request
+
+#X = np.load("winequality-full.npy")
+
+X = np.load("predictors.npy")
+
+training_y_color = np.load("labels_color.npy")
+
+training_y_quality = np.load("labels_quality.npy")
+
+training_y_quality_binary = np.load("labels_quality_binary.npy")
+
+#Check if data loaded in
+print(X.shape)
+
+#Test Wine
+neigh = KNeighborsRegressor(n_neighbors=20)
+neigh.fit(X, training_y_color)
+neigh.fit(X, training_y_quality)
+neigh.fit(X, training_y_quality_binary)
+#test_point = np.array(X)
+#test1 = test_point.reshape(1,-1)
+#print(neigh.predict(test1))
